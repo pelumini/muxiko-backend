0e0831ebfedd937dd3498dd5b1d5dc1efe992d6d
pelumini <pelumini@gmail.com>
2022-07-17 17:03:40 +0100

    added new tracks and file modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from './file/file.module';
+import * as path from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster0.mfnqe.azure.mongodb.net/muxiko?retryWrites=true&w=majority`,
+    ),
+    TrackModule,
+    FileModule,
+  ],
 })
 export class AppModule {}
