3e9ddd192d8b37b5f0325196c43458222db74c35
pelumini <pelumini@gmail.com>
2022-07-18 02:26:23 +0100

    added track module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { FileModule } from './file/file.module';
 import * as path from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster0.mfnqe.azure.mongodb.net/muxiko?retryWrites=true&w=majority`,
